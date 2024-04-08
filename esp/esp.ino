57a72e82b9a3df3e48488f8a06d6f1ed9cd144e2
Tuan Phong <talkphong@gmail.com>
2024-04-08 19:31:14 +0700

    Bai do xe thong minh, ket hop AI, embedded system, nodejs

---
+++ esp/esp.ino
@@ -0,0 +1,89 @@
+#include <WebSocketsClient.h>  //https://github.com/Links2004/arduinoWebSockets
+#include <ESP8266WiFi.h>
+
+#include <Wire.h> // dùng để giao tiếp i2c
+
+#define MAX_DATA_LENGTH 20  // độ dài tối đa của dữ liệu gửi từ slave (20byte)
+#define arduinoIN 8         // Định nghĩa arduino điều khiển chiều vào có địa chỉ 8
+#define SDA_PIN 5           // Định nghĩa chân SDA là chân D1
+#define SCL_PIN 4           // Định nghĩa chân SCL là chân D2
+
+// const char* ssid = "Phong Truong";            //tên wifi
+// const char* password = "cuong1972"; 
+const char* ssid = "Galaxy A235A0C";            //tên wifi
+const char* password = "012345678";           //mật khẩu wifi
+const char* serverAddress = "192.168.50.226"; // Địa chỉ IP của máy chủ Node.js (cmd -> ipconfig ->ipV4address)
+const int serverPort = 3000;                  // Cổng của máy chủ Node.js
+
+WiFiClient wifiClient;
+WebSocketsClient webSocket;
+
+void setup() {
+  Serial.begin(9600); // khởi tạo UART cứng (Để giao tiếp với máy tính, in ra serial monitor)
+  Wire.begin(SDA_PIN, SCL_PIN); // Dùng để giao tiếp i2c với arduino (SDA = D1, SCL = D2)
+
+  // Kết nối tới mạng wifi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Kết nối tới máy chủ Node.js qua WebSockets
+  webSocket.begin(serverAddress, serverPort, "/");
+  webSocket.onEvent(webSocketEvent);  
+}
+
+void loop() {
+  webSocket.loop();
+  receiveArduinoIN();
+}
+
+//các sự kiện websocket
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED: // sự kiện ngắt kết nối
+      Serial.println("Disconnected from server");
+      break;
+    case WStype_CONNECTED: //sự kiện kết nối
+      Serial.println("Connected to server");
+      break;
+    case WStype_TEXT: // NHẬN DỮ LIỆU TỪ SERVER
+      String message = String((char*)payload);
+      Serial.println("From Server: " + message );
+      sendArduino(message); //Sau khi nhận dữ liệu từ sv thì gửi nó đến arduino
+      break;
+  }
+}
+
+// GỬI DỮ LIỆU ĐẾN SERVER
+void sendServer(String message){
+  webSocket.sendTXT(message);
+}
+
+// GỬI DỮ LIỆU ĐẾN ARDUINO
+void sendArduino(String message){
+  Wire.beginTransmission(arduinoIN); // Bắt đầu luồng đến thiết bị số 8
+  Wire.write(message.c_str());  // gửi dữ liệu 
+  Wire.endTransmission();    // Kết thúc
+}
+
+// NHẬN DỮ LIỆU TỪ ARDUINO
+void receiveArduinoIN(){
+  int i = 0;
+  char x, myArray[MAX_DATA_LENGTH] = "";
+  Wire.requestFrom(arduinoIN, MAX_DATA_LENGTH); // Yêu cầu thiết bị số 8 gửi 1 đoạn dữ liệu có độ lớn 20byte
+  do
+  {
+    x = Wire.read();
+    myArray[i] = x;
+    i++;
+  }
+  while (x != '\n');
+  myArray[i - 1] = 0x00;   //thêm tín hiệu kết thúc vào chuỗi khi gặp ký tự \n
+  if( String( myArray)!= ""){
+    Serial.println("From ArduinoIN: " + String(myArray));
+    sendServer(String(myArray)); // gọi hàm gủi đến server
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const PORT = 3000
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+
+// Định nghĩa route cho Express
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Khởi động máy chủ Express trên cổng 3000
+server.listen(PORT, () => {
+  console.log(`Express Server running on PORT ${PORT}`);
+});
+
+const ws = new WebSocket.Server({ server });
+
+var clients = [] // khởi tạo danh sách các cl
+//hàm truyền dữ liệu: gửi dữ liệu cho tất cả cl khác, ngoại từ cl đã gửi dữ liệu lên sv
+function broadcast(socket, message) {
+  console.log(clients.length);
+  for (var i = 0; i < clients.length; i++) {
+      if (clients[i] != socket) {
+          clients[i].send(message.toString());
+      }
+  }
+}
+
+//sự kiện kết nối
+ws.on('connection', function connection(socket) {
+  console.log('A client connected'); //Thông báo đã có 1 client kết nối đến server
+  clients.push(socket); //khi có 1 cl kết nối đến sv, thêm cl đó vào danh sách
+
+  //sự kiện có tin nhắn từ cl gửi đến sv
+  socket.on('message', function incoming(message) {
+    console.log('Received: %s', message);
+    // Xử lý tin nhắn nhận được ở đây (nếu cần)
+    broadcast(socket, message);// Phản hồi lại client
+  });
+
+  //sự kiện kết thúc (cl ngắt kết nối)
+  socket.on('close', function() {
+    console.log('Client disconnected');
+
+    var index = clients.indexOf(socket); // Lấy vị trí của cl đã ngắt kết nốt
+    clients.splice(index, 1); // khi 1 cl ngắt kết nối, loại bỏ cl đó ra khỏi danh sách
+  });
+});
