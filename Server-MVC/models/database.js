677ea67168f46f55e9a9735674b0c282275bcba3
Tuan Phong <talkphong@gmail.com>
2024-04-10 20:09:41 +0700

    server-MVC insert,update,delete khachhang

---
+++ Server-MVC/models/database.js
@@ -0,0 +1,10 @@
+//Database trả về kết nối với cơ sở dữ liệu
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "smart_parking",
+}); 
+db.connect(() => console.log('Da ket noi database !'));
+module.exports = db; 
