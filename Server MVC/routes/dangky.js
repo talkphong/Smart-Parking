0e473b5bff7c5783e73fd014a55123e4aaf53cfa
Tuan Phong <talkphong@gmail.com>
2024-05-15 22:12:12 +0700

    them chuc nang dang ky

--- Server MVC/routes/dangky.js
+++ Server MVC/routes/dangky.js
@@ -9,11 +9,17 @@ router.get('/', function(req, res, next) {
 
 router.post('/xulydangky', function(req, res, next){
   const {tendangnhap, matkhau, hoten, id_khachhang} = req.body
-  const taikhoan = {tendangnhap: tendangnhap, matkhau: matkhau, hoten: hoten}
-  console.log(taikhoan)
-  // db.query(`INSERT INTO nhanvien SET ?`, nhanvien, function (err, data) {
-  //   if (err) throw err;
-  //   res.send("Dang ky thanh cong")
-  // });
+  let sql = `SELECT * FROM khachhang WHERE id_khachhang = ?`;
+  db.query(sql, id_khachhang, function(err, results, fields) {
+    if (results.length > 0) {
+      const taikhoan = {tendangnhap: tendangnhap, matkhau: matkhau, id_khachhang: id_khachhang, phanquyen: "Khách hàng"}
+      db.query('insert into taikhoan SET ?', taikhoan, function(err, data) {
+        if (err) throw err;
+        res.redirect("/dangnhap");
+    });
+    }else {
+      console.log(`Không tìm thấy id khách hàng: ${id_khachhang}`)
+    }
+  })
 });
 module.exports = router;
