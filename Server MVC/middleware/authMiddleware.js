e87edd52ec1fba5a5c642ba0642bef965ea49d90
Tuan Phong <talkphong@gmail.com>
2024-05-18 20:48:21 +0700

    tranh truong hop uer.phanquyen khong ton tai

--- Server MVC/middleware/authMiddleware.js
+++ Server MVC/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 exports.isAdmin = (req, res, next) => {
-    if (req.session.user.phanquyen == 'admin') {
+    if (req.session.user  && req.session.user.phanquyen == 'admin') {
         next();
     } else {
         res.send('Access denied');
@@ -7,7 +7,7 @@ exports.isAdmin = (req, res, next) => {
 };
 
 exports.isStaff = (req, res, next) => {
-    if (req.session.user.phanquyen == 'nhanvien' || req.session.user.phanquyen == 'admin') {
+    if (req.session.user  && (req.session.user.phanquyen == 'nhanvien' || req.session.user.phanquyen == 'admin')) {
         next();
     } else {
         res.send('Access denied');
@@ -15,7 +15,7 @@ exports.isStaff = (req, res, next) => {
 };
 
 exports.isClient = (req, res, next) => {
-    if (req.session.user.phanquyen == 'khachhang' || req.session.user.phanquyen == 'admin') {
+    if (req.session.user  && (req.session.user.phanquyen == 'khachhang' || req.session.user.phanquyen == 'admin')) {
         next();
     } else {
         res.send('Access denied');
